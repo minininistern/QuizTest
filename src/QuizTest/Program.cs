01a8a3da10266920d71141486ce79ec7a8abf419
Christer Carlsson <ccarlsson@outlook.com>
2026-03-18 13:18:33 +0100

    Implement dependency injection for quiz core components and streamline service registration

--- src/QuizTest/Program.cs
+++ src/QuizTest/Program.cs
@@ -1,4 +1,4 @@
-﻿using QuizTest.Core;
+﻿using QuizTest.Core.DependencyInjection;
 using QuizTest.Core.Contracts;
 using QuizTest.Core.Services;
 using QuizTest.Ui.Services;
@@ -6,19 +6,8 @@ using Microsoft.Extensions.DependencyInjection;
 
 var services = new ServiceCollection();
 
-services.AddSingleton<IQuizApiClient>(_ =>
-{
-	var httpClient = new HttpClient
-	{
-		BaseAddress = new Uri("https://opentdb.com"),
-		Timeout = TimeSpan.FromSeconds(30)
-	};
-
-	return new QuizApiClient(httpClient);
-});
+services.AddQuizCore();
 services.AddSingleton<IQuizUi, SpectreQuizUi>();
-services.AddSingleton<IAnswerShuffler, RandomAnswerShuffler>();
-services.AddTransient<QuizGame>();
 
 using var serviceProvider = services.BuildServiceProvider();
 var quizGame = serviceProvider.GetRequiredService<QuizGame>();
